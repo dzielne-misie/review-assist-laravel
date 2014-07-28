06d13fec54a40f65354a5bdca92bb0fce8357e9f
Maciej Iwanowski <kasztelix@gmail.com>
2014-07-28 23:53:21 +0200

    #2 - initial module structure and configuration

---
+++ tests/FakeTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace DzielneMisie\ReviewAssistLaravel\Tests;
+
+use PHPUnit_Framework_TestCase;
+/**
+ * Just a fake test class  used for travis configuration
+ *
+ * @author Maciej Iwanowski <kasztelix@gmail.com>
+ */
+class FakeTest extends PHPUnit_Framework_TestCase
+{
+    public function testPass()
+    {
+        $this->assertSame(1.1, 1.1);
+    }
+    
+    public function testFail()
+    {
+        $this->assertSame(1.0, 1);
+    }
+}
